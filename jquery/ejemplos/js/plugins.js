457d7aeb09d7f753072c7ba009ea0dc841ad7f22
Daniel <danig.fox@gmail.com>
2015-01-27 10:18:14 +0100

    Ejemplos - Actualizado

--- jquery/ejemplos/js/plugins.js
+++ jquery/ejemplos/js/plugins.js
@@ -1,11 +1,12 @@
-var $, jQuery, showCounter;
+var $, jQuery, showCounter, updateCounter;
 $(document).ready(function () {
     'use strict';
 
     $(".parpadear").parpadea();
 
     $("textarea").cuentaCaracteres();
-
+    //$("div").cuentaCaracteres();
+    console.log('hola');
 });
 
 jQuery.fn.parpadea = function () {
@@ -26,10 +27,40 @@ jQuery.fn.cuentaCaracteres = function () {
         elem = jQuery(this);
         counter = $('<p>Caracteres: ' + elem.prop("value").length + '</p>');
         elem.after(counter);
+        elem.data('contador', counter);
 
-        elem.on('keypress', function () {
-            counter.text('Caracteres: ' + elem.prop("value").length);
-        });
+        elem.on('keyup', updateCounter);
     });
     return this;
 };
+
+updateCounter = function () {
+    'use strict';
+    var counter = $(this).data('contador');
+    counter.text('Caracteres: ' + $(this).prop("value").length);
+};
+
+jQuery.fn.creaTip = function (text, options) {
+    'use strict';
+    var config = {
+        speed: 500,
+        animationShow: {width: "show"},
+        animationHide: {opacity: "hide"},
+        tipClass: "tip"
+    };
+    jQuery.extend(config, options);
+
+    this.each(function () {
+        var elem, tip;
+        
+        elem = jQuery(this);
+        tip = jQuery('<div class="' + config.tipClass + '">' + text + '</div>')
+
+        jQuery(document.body).append(tip);
+        elem.data('tipLayer', tip);
+
+        elem.on('mouseenter', function (e) {
+
+        });
+    });
+}
