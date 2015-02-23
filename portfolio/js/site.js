544c0103fff3235b6930b9ee3e4932c6c21f4e95
Daniel <danig.fox@gmail.com>
2015-02-23 09:20:54 +0100

    porfolio v2 (added home)

--- portfolio/js/site.js
+++ portfolio/js/site.js
@@ -2,42 +2,85 @@ var $, jQuery;
 
 $(document).ready(function () {
     'use strict';
-    var distance, duration, locked = false, newSection, fromPosition, toPosition, oldSection,
-        orderNew, orderOld, position, slide, viewportWidth;
+    var distance, duration, locked = false, newSection, fromPosition, toPosition,
+        oldSection, orderNew, orderOld, position, randx, randy, slide, maxHeight,
+        maxWidth, viewportWidth;
 
     // jQuery UI tabs
     $("#cv").tabs();
-    
+
     // jQuery UI datepicker
     $("#fecha_inicio").datepicker({ dateFormat: "dd/mm/yy" });
     $("#fecha_fin").datepicker({ dateFormat: "dd/mm/yy" });
 
+    // Sections order
     oldSection = '#inicio';
-    
-    if (oldSection === '#inicio') {
-        orderOld = 1;
-    }
+    if (oldSection === '#inicio') { orderOld = 1; }
+    if (oldSection === '#trabajos') { orderOld = 2; }
+    if (oldSection === '#sobremi') { orderOld = 3; }
+    if (oldSection === '#contacto') { orderOld = 4; }
 
-    if (oldSection === '#trabajos') {
-        orderOld = 2;
-    }
+    maxHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 0.5;
+    maxWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * 0.7;
 
-    if (oldSection === '#sobremi') {
-        orderOld = 3;
-    }
+    $("#cloud span").each(function () {
 
-    if (oldSection === '#contacto') {
-        orderOld = 4;
-    }
+        randx = Math.round(Math.random() * maxWidth);
+        randy = Math.round(Math.random() * maxHeight);
 
+        $(this).css({
+            'position': 'absolute',
+            'left': randx,
+            'top': randy
+        });
 
-    // Navigation links
+    });
+
+    $("#cloud span").hover(function (e) {
+
+        var oldx, oldy;
+        maxHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) * 0.5;
+        maxWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) * 0.7;
+
+        console.log(e);
+        randx = Math.round(Math.random() * maxWidth);
+        randy = Math.round(Math.random() * maxHeight);
+
+        oldx = $(this).css('left');
+        oldy = $(this).css('top');
+        $(e.target).css({
+            'position': 'absolute'
+        }).animate({
+            color: "#efefef",
+            left: randx,
+            top: randy
+        }, 1000, function () {
+            $(this).animate({
+                fontSize: '48px',
+                left: oldx,
+                top: oldy
+            }, 1000);
+        });
+    }, function (e) {
+        $(e.target).animate({
+            color: "#95afba"
+        }, 1000, function () {
+            $(this).animate({
+                fontSize: '24px'
+            }, 1000);
+        });
+    });
+
+    // Navigation links click event
     $('nav a').bind('click', function (e) {
-        
+
         // Prevent the link action
         e.preventDefault();
 
         if (locked !== true) {
+
+            // Stop the home animations
+            $("#cloud span").stop(true, true);
 
             // Get the block link
             if ((e.target.id.substring(0, 5) === 'link-') && (locked === false)) {
@@ -95,13 +138,13 @@ $(document).ready(function () {
                         fromPosition: fromPosition,
                         toPosition: toPosition,
                         duration: duration
-                    }
+                    };
 
                     $(e.target).slide(options);
-                    
+
                     // Lock input for 1 second
                     locked = true;
-                    setTimeout(function(){
+                    setTimeout(function () {
                         locked = false;
                     }, duration + 1 + 1);
 
